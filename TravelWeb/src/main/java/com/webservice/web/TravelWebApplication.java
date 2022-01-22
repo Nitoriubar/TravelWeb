40232175ad9526ae8e5b77dbffc15afaeebd99ce
김민수 <gimm14851@gmail.com>
2022-01-23 00:48:47 +0900

    fix: spring boox source

--- TravelWeb/src/main/java/com/webservice/web/TravelWebApplication.java
+++ TravelWeb/src/main/java/com/webservice/web/TravelWebApplication.java
@@ -2,10 +2,10 @@ package com.webservice.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TravelWebApplication {
-	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
 		return application.sources(TravelWebApplication.class);
 	}
