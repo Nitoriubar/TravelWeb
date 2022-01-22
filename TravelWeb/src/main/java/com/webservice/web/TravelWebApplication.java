3123c4c3b032be7e27f8b0ea669d07637e78b752
root <root@ip-172-31-38-211.ap-northeast-2.compute.internal>
2022-01-22 13:52:38 +0000

    fix: config updating 현재 import 오류로 인한 수정 진행중

--- TravelWeb/src/main/java/com/webservice/web/TravelWebApplication.java
+++ TravelWeb/src/main/java/com/webservice/web/TravelWebApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TravelWebApplication {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(TravelWebApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelWebApplication.class, args);
