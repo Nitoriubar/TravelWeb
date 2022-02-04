c4328de0e1b82e6c2fb5eb6272956ce82d3779ca
dana <seonhui9759@naver.com>
2022-02-04 16:21:45 +0900

    kakao login 연동

--- TravelWeb/src/main/java/com/webservice/web/TravelWebApplication.java
+++ TravelWeb/src/main/java/com/webservice/web/TravelWebApplication.java
@@ -2,12 +2,25 @@ package com.webservice.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TravelWebApplication {
+public class TravelWebApplication extends SpringBootServletInitializer{
 
+	void contextLoads() {
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TravelWebApplication.class, args);
 	}
+	
+	@Override
+	 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+ 
+        return application.sources(TravelWebApplication.class);
+ 
+    }
 
 }
